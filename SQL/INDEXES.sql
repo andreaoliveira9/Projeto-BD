2c44aef8b37b8789c035a7c69d78808982a35a8c
André Oliveira <andreaoliveira@ua.pt>
2023-06-01 19:57:58 +0100

    update

--- SQL/INDEXES.sql
+++ SQL/INDEXES.sql
@@ -4,7 +4,6 @@ drop index IF EXISTS seacrhContractStartDate on NBA.[Contract];
 drop index IF EXISTS searchContractEndDate on NBA.[Contract];
 drop index IF EXISTS searchPersonName on NBA.Person;
 drop index IF EXISTS seacrhTeamName on NBA.Team;
-drop index IF EXISTS seacrhTeamID on NBA.Team;
 drop index IF EXISTS seacrhPlayerTeam on NBA.Player;
 drop index IF EXISTS seacrhGameHomeTeamScore on NBA.Game;
 drop index IF EXISTS seacrhGameAwayTeamScore on NBA.Game;
@@ -19,7 +18,6 @@ create index searchPersonName on NBA.Person ([Name]);
 
 -- Criação de indexes na tabela NBA.Team
 create index seacrhTeamName on NBA.Team ([Name]);
-create index seacrhTeamID on NBA.Team (ID);
 
 -- Criação de indexes na tabela NBA.Player
 create index seacrhPlayerTeam on NBA.Player (Team_ID);
